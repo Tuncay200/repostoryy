808e14b2219e856ea13ac0b30d17f9a7b6173eb9
Tuncay200 <tuncayadisirinli528@gmail.com>
2024-05-17 20:27:32 +0400

    fghj

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public  static void main(){
         int a=5;
         int b=3;
-        int temp= a +b;
+        int temp= a +b;fdehr
         System.out.println(temp);
     }
 }
